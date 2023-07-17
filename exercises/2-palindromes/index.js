ab4a388a715567203b82e6ad500121ac59cc88b9
ervinMamutov <ervin.news@gmail.com>
2023-07-17 12:47:16 +0200

    finish 2-palindromes

--- exercises/2-palindromes/index.js
+++ exercises/2-palindromes/index.js
@@ -1,8 +1,28 @@
 /**
- *
+ * determines whether or not a given string is a palindrome
+ * @param {string} [text=''] specified text
+ * @returns {boolean} if text - palindrome -> true, else -> false;
  */
-const palindromes = () => {
+const palindromes = (text = '') => {
+  const textToLowerCase = text.toLowerCase();
+  const textSpecified = text.toLowerCase().replace(/[ .,!]/g, '');
+  /* 
+  const textSpecified = textToLowerCase
+    .split(' ')
+    .join('')
+    .split('.')
+    .join('')
+    .split(',')
+    .join('')
+    .split('!')
+    .join('');
+     */
+  const textResult = textSpecified.split('').reverse().join('');
 
+  console.log(textSpecified);
+  console.log(textResult);
+
+  return textResult === textSpecified ? true : false;
 };
 
 module.exports = palindromes;
