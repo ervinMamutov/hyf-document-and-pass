a9b12cd0964073a251f412a0c189ef33d546ace2
ervinMamutov <ervin.news@gmail.com>
2023-07-17 11:15:49 +0200

    finish 2-leap-years

--- exercises/2-leap-years/index.js
+++ exercises/2-leap-years/index.js
@@ -1,8 +1,26 @@
 /**
- *
+ *  determines a leap year
+ * @param {number} [year] specified year
+ * @returns {boolean} if year's leap -> true, else -> false
  */
-const leapYears = function () {
+const leapYears = function (year) {
+  /* 
 
-}
+  yes :  The year must be evenly divisible by 4;
+  no: If the year can also be evenly divided by 100, it is not a leap year;
+	unless...
+  yes: The year is also evenly divisible by 400. Then it is a leap year.
+ */
 
-module.exports = leapYears
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = leapYears;
